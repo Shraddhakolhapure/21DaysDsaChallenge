2a1a8a9100ca4a0b3bb8c646f61ca5f8a9383c17
Shraddha Kolhapure <106257764+Shraddhakolhapure@users.noreply.github.com>
2023-09-29 18:54:27 +0530

    ToeplitzMatrix.java

--- ToeplitzMatrix.java
+++ ToeplitzMatrix.java
@@ -1,33 +1,31 @@
 import java.util.Scanner;
 
 public class ToeplitzMatrix {
-    public static void main(String[] args)
-    {
-    Scanner scn = new Scanner(System.in);
-    System.out.println("Enter the dimensions of the array: ");
-    int n = scn.nextInt();
-    int m = scn.nextInt();
-    int[][] mat = new int[n][m];
-    System.out.println("Enter the elements of the array: ");
-    for (int i = 0; i < n; i++)
-     {
-        for (int j = 0; j < m; j++) 
-        {
-            mat[i][j] = scn.nextInt();
-        }
-     }
-    for (int i = 1; i < n; i++)
-     { 
-        for (int j = 1; j < m; j++)
-         {
-            if(mat[i][j] != mat[i-1][j-1])
-            {
-              System.out.println(false);
-              return;
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the dimensions of the array: ");
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] mat = new int[n][m];
+        System.out.println("Enter the elements of the array: ");
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                mat[i][j] = scn.nextInt();
             }
-         }
-     }
-    System.out.println(true);
-    scn.close();   
+        }
+        
+        for (int i = 1; i < n; i++) { 
+            for (int j = 1; j < m; j++) {
+                if (mat[i][j] != mat[i - 1][j - 1]) {
+                    System.out.println("false");
+                    scn.close();
+                    return;
+                }
+            }
+        }
+        
+        System.out.println("true");
+        scn.close();
     }
 }
